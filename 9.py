1f63f1b407f3d118ef27f6896a441e1b9b101030
Liheng <lihengcao@gmail.com>
2021-03-31 23:56:01 -0500

    q9 done

---
+++ 9.py
@@ -0,0 +1,9 @@
+# brute force solution
+
+for c in range(999): # can't be 999 + 1 + 1
+    for b in range(c):
+        for a in range(b):
+            if a + b + c > 1000:
+                break
+            if a + b + c == 1000 and a**2 + b**2 == c**2:
+                print(a*b*c)
