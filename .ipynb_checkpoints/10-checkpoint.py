16c67ad1d5f1f74a3cbe548ca322967d3bf9c966
Liheng <lihengcao@gmail.com>
2021-08-06 11:10:51 -0400

    ipynb checkpoint

---
+++ .ipynb_checkpoints/10-checkpoint.py
@@ -0,0 +1,15 @@
+up_to = int(2E6)  # 2 million
+
+table = [True for _ in range(up_to + 1)]
+
+primes = []
+
+i = 2
+while i < len(table): # Sieve of Eratosthenes
+    if table[i]:
+        primes.append(i)
+        for j in range(i, len(table), i):
+            table[j] = False
+    i += 1
+
+print(sum(primes))
