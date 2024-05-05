4c37dd68a424e7cf21c5016f5996d0daebf33f10
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-05-05 18:31:25 -0400

    start 20

---
+++ Project Euler/20.py
@@ -0,0 +1,6 @@
+# %% [markdown]
+# # Observations
+# multiplying by 10 doesn't change the sum. We can skip 10. In terms of effect on the digit sum, multiplying by 20 is the same as 2 * 10, which is equivalent to just multiplying by 2. 
+
+# %%
+print(2)
