9ff24196d56cddf250b2ad9aafd2e9e6d34fcc68
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-08-06 13:05:23 -0400

    second try

--- Codeforces/Round 962 Div 4/d.py
+++ Codeforces/Round 962 Div 4/d.py
@@ -4,8 +4,7 @@ for _ in range(int(input())):
 
 
     cache = {}
-    def solve(i_s, i_t) -> bool:
-        # print(i_s, i_t)
+    def solve(s: str, t: str, cache: dict[tuple[int, int], bool], i_s: int, i_t: int) -> bool:
         if i_t >= len(t):
             return True
 
@@ -16,52 +15,28 @@ for _ in range(int(input())):
             return cache[(i_s, i_t)]
 
         if s[i_s] == t[i_t]:
-            a = solve(i_s + 1, i_t + 1)
+            a = solve(s, t, cache, i_s + 1, i_t + 1)
             cache[(i_s, i_t)] = a
             return a
 
-        if s[i_s] != '?':
-            return False
-        
-        orig = s[i_s]
-        s[i_s] = t[i_t]
-        if solve(i_s + 1, i_t + 1):
+        if solve(s, t, cache, i_s + 1, i_t):
             cache[(i_s, i_t)] = True
             return True
-        s[i_s] = orig
-        if solve(i_s + 1, i_t):
-            cache[(i_s, i_t)] = True
-            return True
+
+        if s[i_s] == '?':
+            s[i_s] = t[i_t]
+            if solve(s, t, cache, i_s + 1, i_t + 1):
+                cache[(i_s, i_t)] = True
+                return True
+            s[i_s] = '?'
+
         
         cache[(i_s, i_t)] = False
         return False
 
         
-    if solve(0, 0):
+    if solve(s, t, cache, 0, 0):
         print("YES")
         print("".join(c if c != '?' else 'a' for c in s))
     else:
         print("NO")
-
-
-    # i_s, i_t = 0, 0
-
-    # while i_t < len(t):
-    #     if s[i_s] == t[i_t]:
-    #         i_s += 1
-    #         i_t += 1
-    #         continue
-
-    #     if s[i_s] != '?':
-    #         print("NO")
-    #         break
-
-    #     s[i_s] = t[i_t]
-    #     i_s += 1
-    #     i_t += 1
-    # else:
-    #     print("YES")
-    #     print("".join(c if c != '?' else 'a' for c in s))
-
-
-
