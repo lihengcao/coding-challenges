0711b9fa69ec228f8f7ec8b52edd826db451f8b9
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-12-09 20:17:49 -0500

    day 9 in py

---
+++ Advent of Code/2024/9 py/1.py
@@ -0,0 +1,52 @@
+def read_input():
+    return input()
+
+
+def main():
+    file = read_input()
+
+    print(calc_final_checksum(file))
+
+
+# hey... it fits in memory
+def calc_final_checksum(file: str) -> int:
+    expanded_disk = []
+
+    for i in range(len(file)):
+        if i % 2 == 0:
+            expanded_disk.extend([i//2] * int(file[i]))
+        else:
+            expanded_disk.extend([None] * int(file[i]))
+
+    print(expanded_disk)
+
+    left, right = 0, len(expanded_disk) - 1
+
+    while left < right:
+        if expanded_disk[left] is not None:
+            left += 1
+        elif expanded_disk[right] is None:
+            right -= 1
+        else:
+            expanded_disk[left] = expanded_disk[right]
+            expanded_disk[right] = None
+            left += 1
+            right -= 1
+
+    print(expanded_disk)
+
+    count = 0
+    for ind, id in enumerate(expanded_disk):
+        if id is None:
+            return count
+
+        count += ind * id
+
+    return count
+        
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Advent of Code/2024/9 py/2.py
@@ -0,0 +1,87 @@
+def read_input():
+    return input()
+
+
+def main():
+    file = read_input()
+
+    print(calc_final_checksum(file))
+
+
+# hey... it fits in memory
+def calc_final_checksum(compressed_file: str) -> int:
+    expanded_disk = []
+    free_space = []
+    files = []
+
+    for i in range(len(compressed_file)):
+        val = int(compressed_file[i])
+        if i % 2 == 0:
+            files.append([len(expanded_disk), val])
+            expanded_disk.extend([i//2] * val)
+        else:
+            free_space.append([len(expanded_disk), val])
+            expanded_disk.extend([None] * val)
+
+    print("free", free_space)
+    print("files", files)
+    print("disk", expanded_disk)
+
+    for files_ind in range(len(files) - 1, -1, -1):
+        move_to_free_ind = None
+        file_loc, file_size = files[files_ind]
+        for free_space_ind in range(len(free_space)):
+            free_loc, free_space_size = free_space[free_space_ind]
+
+            # potentiali bsearch opti
+            if file_loc < free_loc:
+                continue
+
+            if free_space_size >= file_size:
+                # in theory files_ind should still be accesible after the break, but let me be clear!
+                move_to_free_ind = free_space_ind
+                break
+        
+        if move_to_free_ind is not None:
+            move(expanded_disk, free_space, files, move_to_free_ind, files_ind)
+
+    print("disk", expanded_disk)
+
+    count = 0
+    for ind, id in enumerate(expanded_disk):
+        if id is None:
+            continue
+
+        count += ind * id
+
+    return count
+        
+
+def move(expanded_disk: list[int], free_space, files, free_space_ind, files_ind) -> None:
+    file_loc, file_size = files[files_ind]
+    free_loc, free_space_size = free_space[free_space_ind]
+    for i in range(file_size):
+        expanded_disk[free_loc + i] = files_ind
+        expanded_disk[file_loc + i] = None
+
+    free_space[free_space_ind][1] -= file_size
+
+    # might not be an optimization
+    # if free_space[free_space_ind][1] == 0:
+    #     free_space.pop(free_space_ind)
+    # else:
+    
+    free_space[free_space_ind][0] += file_size
+
+
+    
+
+
+    
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
