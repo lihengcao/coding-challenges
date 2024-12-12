78aaf66ec02bf4142ecf80353347e97209a65480
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-12-11 21:44:50 -0500

    day 11 in py

---
+++ Advent of Code/2024/11 py/1.py
@@ -0,0 +1,30 @@
+def read_input():
+    return [s for s in input().split(' ') if s != '']
+
+def main():
+    stones = read_input()
+    
+    print(blink(stones))
+
+def blink(stones: list[str]) -> int:
+    for i in range(25):
+        new = []
+
+        for stone in stones:
+            if stone == "0":
+                new.append("1")
+            elif len(stone) % 2 == 0:
+                N = len(stone) // 2
+                left, right = stone[:N], stone[N:]
+                right = str(int(right))
+                new.extend([left, right])
+            else:
+                new.append(str(int(stone) * 2024))
+
+        stones = new
+        print(i, len(stones))
+
+    return len(stones)
+
+if __name__ == "__main__":
+    main()
---
+++ Advent of Code/2024/11 py/2.py
@@ -0,0 +1,51 @@
+from math import log10
+from collections import defaultdict
+
+def read_input():
+    return [int(s) for s in input().split(' ') if s != '']
+
+def main():
+    ordered_stones = read_input()
+
+    stones = defaultdict(int)
+
+    for stone in ordered_stones:
+        stones[stone] += 1
+    
+    print(blink(stones))
+
+def blink(stones: dict[int, int]) -> int:
+    for i in range(25):
+        new = defaultdict(int)
+
+        for stone, count in stones.items():
+            if stone == 0:
+                new[1] += count
+            elif (N := count_digits(stone)) % 2 == 0:
+                left, right = split_number(stone, N)
+
+                new[left] += count
+                new[right] += count
+            else:
+                new[stone * 2024] += count
+
+        stones = new
+        print(i, sum(stones.values()))
+
+    return sum(stones.values())
+
+def count_digits(n: int) -> int:
+    return int(log10(n)) + 1
+
+def split_number(n: int, length: int) -> tuple[int, int]:
+    M = length // 2
+
+    cutoff = 10 ** M
+
+    left = n // cutoff
+    right = n % cutoff
+
+    return left, right
+
+if __name__ == "__main__":
+    main()
