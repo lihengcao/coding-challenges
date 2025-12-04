71009db29e8d6d10d68a36f7a60c6ef84d9a6db1
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2025-12-03 19:00:48 -0500

    2025 day 1

---
+++ Advent of Code/2025/1/1.py
@@ -0,0 +1,30 @@
+def main():
+    zeros = 0
+
+    ops = []
+
+    while True:
+        try:
+            line = input()
+            dir, n = line[0], int(line[1:])
+            ops.append((dir, n))
+        except EOFError:
+            break
+
+    p = 50
+
+    for dir, n in ops:
+        if dir == 'L':
+            p -= n
+        elif dir == 'R':
+            p += n
+
+        p %= 100
+        
+        if p == 0:
+            zeros += 1
+
+    print(zeros)
+
+if __name__ == "__main__":
+    main()
---
+++ Advent of Code/2025/1/2.py
@@ -0,0 +1,40 @@
+def main():
+    zeros = 0
+
+    ops = []
+
+    while True:
+        try:
+            line = input()
+            dir, n = line[0], int(line[1:])
+            ops.append((dir, n))
+        except EOFError:
+            break
+
+    p = 50
+
+    for dir, n in ops:
+        zeros += n // 100
+        n %= 100
+
+        left = 100 if p == 0 else p
+        right = 100 - p
+
+        if dir == 'L':
+            p -= n
+        elif dir == 'R':
+            p += n
+        else:
+            raise ValueError
+
+        # print(dir, n, p, left, right)
+        if (dir == 'L' and n >= left) or (dir == 'R' and n >= right):
+            # print('---', dir, n, p)
+            zeros += 1
+
+        p %= 100
+
+    print(zeros)
+
+if __name__ == "__main__":
+    main()
